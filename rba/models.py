a7bdd2a66e76363c114a7be842a0d562ed2a9aab
ElisabethDemaio <elisabeth.demaio@gmail.com>
2022-06-15 16:51:58 +0200

    new graph color 4.0

--- rba/models.py
+++ rba/models.py
@@ -104,8 +104,8 @@ class CSphase2(ClusterableModel):
         pos.update( (node[0], (2, i)) for i, node in enumerate(p_nodes) ) # put nodes from Y at x=2
         pos.update( (node[0], (3, i)) for i, node in enumerate(e_nodes) ) # put nodes from X at x=3
         
-        inferno = px.colors.sequential.Burg
-        viridis = px.colors.sequential.Burg
+        inferno = px.colors.sequential.Bluered
+        viridis = px.colors.sequential.Bluered
 
         weight = []
         edge_traces = []
