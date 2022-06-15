f052ffc11930f6bf77b6aeee5252f299b0f32bb0
ElisabethDemaio <elisabeth.demaio@gmail.com>
2022-06-15 16:54:51 +0200

    new graph color 5.0

--- rba/models.py
+++ rba/models.py
@@ -130,7 +130,7 @@ class CSphase2(ClusterableModel):
             c = c + 1
             edge_traces.append(go.Scatter(
                 x=edge_x, y=edge_y,
-                line=dict(width=1.5, color=color[c] ), 
+                line=dict(width=0.9, color=color[c] ), 
                 hoverinfo='none',
                 mode='lines'))
                 
@@ -156,7 +156,7 @@ class CSphase2(ClusterableModel):
 
             edge_traces2.append(go.Scatter(
                 x=edge_x2, y=edge_y2,
-                line=dict(width=1.5, color=color2[e]),
+                line=dict(width=0.9, color=color2[e]),
                 hoverinfo='none',
                 mode='lines'))
 
