53d83b82d9416e998a86210af30937adaa7a41c1
ElisabethDemaio <elisabeth.demaio@gmail.com>
2022-06-16 09:56:24 +0200

    new context graph

--- rba/models.py
+++ rba/models.py
@@ -89,11 +89,11 @@ class CSphase2(ClusterableModel):
         _p1 = [(up.pressure_list.code, up.pressure_list.label) for up in phase.pathup_objects.all()]
         _p2 = [(pe.pressure_list.code, pe.pressure_list.label) for pe in phase.pathpe_objects.all()]
         _e = [(pe.env_list.code, pe.env_list.label) for pe in phase.pathpe_objects.all()]
-        w_data = [(w.use.code, w.pres.code, w.weight) for w in Weight.objects.all()]
-        s_data = [(s.pres.code, s.env.code, s.sensitivity) for s in Sensitivity.objects.all()]
+        # w_data = [(w.use.code, w.pres.code, w.weight) for w in Weight.objects.all()]
+        # s_data = [(s.pres.code, s.env.code, s.sensitivity) for s in Sensitivity.objects.all()]
     
-        # w_data = [(w.use.code, w.pres.code, w.weight) for w in Weight.objects.filter(context=1)]
-        # s_data = [(s.pres.code, s.env.code, s.sensitivity) for s in Sensitivity.objects.filter(context=1)]
+        w_data = [(w.use.code, w.pres.code, w.weight) for w in Weight.objects.filter(context=8)]
+        s_data = [(s.pres.code, s.env.code, s.sensitivity) for s in Sensitivity.objects.filter(context=8)]
 
         u_nodes = list(set(_u))
         p_nodes = list(set(_p1 + _p2))
